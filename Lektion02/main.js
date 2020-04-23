37d1c6a270ca8579f1a4bfa47de332aed0a92d53
doldsimo <simon.dold@hs-furtwangen.de>
2020-04-23 14:22:51 +0200

    aktualisiere Hallo Welt Meldung

---
+++ Lektion02/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var massage = "Hallo Welt!";
+alert(massage);
+console.log(massage);
+//# sourceMappingURL=main.js.map
--- Lektion02/main.ts
+++ Lektion02/main.ts
@@ -1,1 +1,4 @@
-console.log("Hallo Welt");
+var massage = "Hallo Welt!";
+
+alert(massage)
+console.log(massage);
