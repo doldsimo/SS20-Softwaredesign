daf84f6b2ece39b424753b40c95dbab5371f88a3
Patrick Kaserer <48802394+Z3r0cks@users.noreply.github.com>
2020-05-13 10:13:30 +0200

    Update main.ts

--- UML-Zufallsgedicht-Umsetzung/main.ts
+++ UML-Zufallsgedicht-Umsetzung/main.ts
@@ -1,14 +1,16 @@
+// Namespace PascalCase
 namespace main {
     let subjects: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let verbs: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objects: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren"];
-
+    
     let kMax: number = subjects.length;
 
     for (let k = 0; k < kMax; k++) {
         console.log(getVers(subjects, verbs, objects));
     }
     
+    // Übergabewert im underscore (z.B: _name)
     function getVers(subjects: string[], verbs: string[], objects: string[]): string {
         let vers: string = stringSelector(randomInteger(0, subjects.length), subjects)+ " " + stringSelector(randomInteger(0, verbs.length), verbs) + " " +stringSelector(randomInteger(0, objects.length), objects)
 
